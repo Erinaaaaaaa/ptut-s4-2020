8e201bd418795b9556d7a6e00a42afca082a8297
Edouard <edouard.varin2000@gmail.com>
2020-03-23 13:27:21 +0100

    Controleur bo

--- Client/src/twist/Controleur.java
+++ Client/src/twist/Controleur.java
@@ -3,25 +3,18 @@ package twist;
 import twist.ihm.Fenetre;
 import twist.metier.Pont;
 import twist.ihm.launcher.*;
-public class Controleur {
-
-	private Fenetre fenetre;
-
-	public Controleur(){
-		new Pont();
-		//new Launcheur(this);
-
-		this.fenetre = new Fenetre(this);
-	}
 
 public class Controleur
 {
     private Pont pont;
+    private Fenetre fenetre;
 
     public Controleur()
     {
         this.pont = new Pont();
         new Launcheur(this);
+        this.fenetre = new Fenetre(this);
+
     }
 
     public static void main(String[] args)
