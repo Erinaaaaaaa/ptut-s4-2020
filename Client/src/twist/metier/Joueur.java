ca21458ac5e18b5d9e95ab45d6387965af76f72b
Jeremiidesu <jerem.auz@gmail.com>
2020-03-23 14:22:37 +0100

    Update Pont

--- Client/src/twist/metier/Joueur.java
+++ Client/src/twist/metier/Joueur.java
@@ -3,14 +3,14 @@ package twist.metier;
 public class Joueur
 {
     private String nom;
+    private int nbLocks;
 
-    public Joueur(String nom)
+    public Joueur(String nom, int nbLocks)
     {
         this.nom = nom;
+        this.nbLocks = nbLocks;
     }
 
-    public String getNom()
-    {
-        return nom;
-    }
+    public String getNom() { return nom; }
+    public int getNbLocks() { return this.nbLocks; }
 }
--- Client/src/twist/metier/Pont.java
+++ Client/src/twist/metier/Pont.java
@@ -1,5 +1,7 @@
 package twist.metier;
 
+import twist.Controleur;
+
 public class Pont
 {
     private static final int MIN_LIGNES   = 5;
@@ -8,6 +10,10 @@ public class Pont
     private static final int MAX_LIGNES   = 20;
     private static final int MAX_COLONNES = 20;
 
+    private static final int NB_LOCKS_DEFAULT = 20;
+
+    private Controleur ctrl;
+
     private int largeur;
     private int hauteur;
 
@@ -15,10 +21,20 @@ public class Pont
 
     private Joueur[] joueurs;
 
-    public Pont()
+    public Pont(Controleur ctrl, String[] nomsJoueurs)
     {
-        this.largeur = (int) (Math.random() * MAX_LIGNES   - MIN_LIGNES)   + MIN_LIGNES;
-        this.hauteur = (int) (Math.random() * MAX_COLONNES - MIN_COLONNES) + MIN_COLONNES;
+        this(ctrl, nomsJoueurs,
+                (int) (Math.random() * MAX_LIGNES   - MIN_LIGNES)   + MIN_LIGNES,
+                (int) (Math.random() * MAX_COLONNES - MIN_COLONNES) + MIN_COLONNES,
+                NB_LOCKS_DEFAULT);
+    }
+
+    public Pont(Controleur ctrl, String[] nomsJoueurs, int largeur, int hauteur, int nbLocks)
+    {
+        this.ctrl = ctrl;
+
+        this.largeur = largeur;
+        this.hauteur = hauteur;
 
         this.conteneurs = new Conteneur[largeur][hauteur];
 
@@ -41,6 +57,11 @@ public class Pont
             if (y < hauteur - 1 && x > 0          ) c.setVoisin(6, this.conteneurs[x - 1][y + 1]);
             if (                   x > 0          ) c.setVoisin(7, this.conteneurs[x - 1][y    ]);
         }
+
+        this.joueurs = new Joueur[nomsJoueurs.length];
+
+        for (int i = 0; i < nomsJoueurs.length; i++)
+            this.joueurs[i] = new Joueur(nomsJoueurs[i], nbLocks);
     }
 
     public Conteneur[][] getPlateau()
@@ -60,11 +81,5 @@ public class Pont
     public Joueur getJoueur(int i)
     {
         return null;
-    }
-
-    public static void main(String[] args)
-    {
-        Pont pont = new Pont();
-        System.out.println("break");
     }
 }
