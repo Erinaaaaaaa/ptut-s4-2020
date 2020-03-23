a74ec2d869b9c57540b2cd2696f8589f3a9de270
Jeremiidesu <jerem.auz@gmail.com>
2020-03-23 13:13:01 +0100

    Store instance of Pont in Controleur

--- Client/src/twist/Controleur.java
+++ Client/src/twist/Controleur.java
@@ -2,13 +2,19 @@ package twist;
 
 import twist.metier.Pont;
 import twist.ihm.launcher.*;
-public class Controleur {
-	public Controleur(){
-		new Pont();
-		new Launcheur(this);
-	}
 
-public static void main(String[] args) {
-		new Controleur();
-	}
+public class Controleur
+{
+    private Pont pont;
+
+    public Controleur()
+    {
+        this.pont = new Pont();
+        new Launcher(this);
+    }
+
+    public static void main(String[] args)
+    {
+        new Controleur();
+    }
 }
