be07e813c495d05e94089d3aaf7f6608da09884c
Edouard <edouard.varin2000@gmail.com>
2020-03-23 13:25:35 +0100

    Avancement IHM

--- Client/src/twist/Controleur.java
+++ Client/src/twist/Controleur.java
@@ -1,7 +1,18 @@
 package twist;
 
+import twist.ihm.Fenetre;
 import twist.metier.Pont;
 import twist.ihm.launcher.*;
+public class Controleur {
+
+	private Fenetre fenetre;
+
+	public Controleur(){
+		new Pont();
+		//new Launcheur(this);
+
+		this.fenetre = new Fenetre(this);
+	}
 
 public class Controleur
 {
--- Client/src/twist/ihm/Fenetre.java
+++ Client/src/twist/ihm/Fenetre.java
@@ -3,10 +3,12 @@ package twist.ihm;
 import twist.Controleur;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Fenetre extends JFrame
 {
     private Controleur ctrl;
+    private PanelJoueur[] pJ;
 
     public Fenetre(Controleur c)
     {
@@ -15,15 +17,25 @@ public class Fenetre extends JFrame
         this.setSize(900,600);
         this.ctrl = c;
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setVisible(true);
 
         /*-------------------*/
         /* PANEL DES JOUEURS */
         /*-------------------*/
 
-        
+        //TODO: faire en fonction du nombre de joueur
 
-        
+        this.pJ[0] = new PanelJoueur(this.ctrl, "joueurR");
+        this.pJ[1] = new PanelJoueur(this.ctrl, "joueurV");
+        this.pJ[2] = new PanelJoueur(this.ctrl, "joueurB");
+        this.pJ[3] = new PanelJoueur(this.ctrl, "joueurJ");
+
+        this.add(this.pJ[0], BorderLayout.WEST);
+        this.add(this.pJ[1], BorderLayout.EAST);
+        this.add(this.pJ[2], BorderLayout.NORTH);
+        this.add(this.pJ[3], BorderLayout.SOUTH);
+
+        this.setVisible(true);
+
     }
 
 
